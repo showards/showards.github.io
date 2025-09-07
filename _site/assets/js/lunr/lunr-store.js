d1ff2f05abe535b5e273f2b7914491d903fcf4e0
Chris Olave <chris@techsupport.net>
2025-09-07 13:53:14 -0700

    - removed all files

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Introduction to Balances",
-        "excerpt":"In a complex world of business and finance, understanding a company’s financial standing is paramount for all stakeholders: management, investors, creditors, and employees. While various reports offer pieces of the puzzle, two statements stand out as fundamentals in financial reporting: the balance sheet and the cash flow statement. These documents...","categories": [],
-        "tags": [],
-        "url": "/balances/introduction.html",
-        "teaser": null
-      }]
